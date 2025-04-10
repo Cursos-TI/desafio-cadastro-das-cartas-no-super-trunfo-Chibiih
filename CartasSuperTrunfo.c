2d5954e100850073318a2ce0ac1540008fa5459f
Scheilla Vieira <scheilla.vet04@gmail.com>
2025-04-09 22:25:10 -0300

    arrumando alguns erros

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,20 +5,9 @@
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das Cartas
 
-// o que é necessário para montar a estrutura basica
-
-// Estado: Uma letra de 'A' a 'H' (representando um dos oito estados). Tipo: char
-// Código da Carta: A letra do estado seguida de um número de 01 a 04 (ex: A01, B03). Tipo: char[] (um array de caracteres, ou string)
-// Nome da Cidade: O nome da cidade. Tipo: char[] (string)
-// População: O número de habitantes da cidade. Tipo: int
-// Área (em km²): A área da cidade em quilômetros quadrados. Tipo: float
-// PIB: O Produto Interno Bruto da cidade. Tipo: float
-// Número de Pontos Turísticos: A quantidade de pontos turísticos na cidade. Tipo: int
-
+//Informações importantes
 // %d: Imprime um inteiro no formato decimal.
-// %i: Equivalente a %d.
 // %f: Imprime um número de ponto flutuante no formato padrão.
-// %e: Imprime um número de ponto flutuante na notação científica.
 // %c: Imprime um único caractere.
 // %s: Imprime uma cadeia (string) de caracteres.
 
@@ -26,70 +15,55 @@ int main() {
     // Definindo as variaveis
     char estadoUm, estadoDois, codigoCartaUm[4], codigoCartaDois[4], nomeCidadeUm[35], nomeCidadeDois[35];
     int populacaoUm, populacaoDois, pontosTuristicosUm, pontosTuristicosDois;
-    float areaUm, areaDois, pibUm, pibDois;
+    float areaUm, areaDois, pibUm, pibDois, densidadeUm, densidadeDois, pibcapUm, pibcapDois;
 
-    // Coletando os dados pelo usuário e imprimindo na tela os dados colidos.
+    // Coletando os dados pelo usuário
 
     printf("Inserção de dados para o jogo Super Trunfo \n \n");
 
     printf("Insira o primeiro estado da carta 1 (Uma letra entre A - H):");
     scanf("%c", &estadoUm);
-    printf("Estado um: %c \n", estadoUm);
     
     printf("Insira o código da carta 1 (Sendo a letra do estado seguida de um número de 01 a 04): ");
     scanf("%s", &codigoCartaUm);
-    printf("Código da carta 1: %s \n", codigoCartaUm);
     
     printf("Insira o nome da Cidade da carta 1: ");
     scanf("%s", &nomeCidadeUm);
-    printf("Nome da cidade 1: %s \n", nomeCidadeUm);
     
     printf("Insira a quantidade da população da carta 1: ");
     scanf("%d", &populacaoUm);
-    printf("População 1:  %d \n", populacaoUm);
     
     printf("Insira a area da carta 1: ");
     scanf("%f", &areaUm);
-    printf("Área 1: %f \n", areaUm);
 
     printf("Insira o PIB da carta 1: ");
     scanf("%f", &pibUm);
-    printf("PIB 1: %f \n", pibUm);
 
     printf("Insira o numero de pontos turisticos da carta 1: ");
     scanf("%d", &pontosTuristicosUm);
-    printf("Pontos Turisticos 1: %d \n", pontosTuristicosUm);
-
 
     printf("\n\nAgora vamos inserir os dados da Carta 2 \n\nInsira o primeiro estado da carta 2: ");
     // não sei porque ele pula pro proximo então arrumei o erro dessa forma...
     scanf("%c", &estadoDois);
     scanf("%c", &estadoDois);
-    printf("Estado dois: %c \n", estadoDois);
     
     printf("Insira o código da carta 2: ");
     scanf("%s", &codigoCartaDois);
-    printf("Código da carta 2: %s \n", codigoCartaDois);
     
     printf("Insira o nome da Cidade da carta 2: ");
     scanf("%s", &nomeCidadeDois);
-    printf("Nome da cidade 2: %s \n", nomeCidadeDois);
     
     printf("Insira a quantidade da população da carta 2: ");
     scanf("%d", &populacaoDois);
-    printf("População 2:  %d \n", populacaoDois);
     
     printf("Insira a area da carta 2: ");
     scanf("%f", &areaDois);
-    printf("Área 2: %f \n", areaDois);
 
     printf("Insira o PIB da carta 2: ");
     scanf("%f", &pibDois);
-    printf("PIB 2: %f \n", pibDois);
 
     printf("Insira o numero de pontos turisticos da carta 2: ");
     scanf("%d", &pontosTuristicosDois);
-    printf("Pontos Turisticos 2: %d \n", pontosTuristicosDois);
 
 // Mostrando as informações inseridas nas cartas.
 
@@ -99,8 +73,8 @@ int main() {
     printf("Codigo: %s \n", codigoCartaUm);
     printf("Cidade: %s \n", nomeCidadeUm);
     printf("População: %d \n", populacaoUm);
-    printf("Área: %f km² \n", areaUm);
-    printf("PIB: %f bilhões de reais \n", pibUm);
+    printf("Área: %.2f km² \n", areaUm);
+    printf("PIB: %.2f bilhões de reais \n", pibUm);
     printf("Número de pontos turisticos: %d \n", pontosTuristicosUm);
     // Adicionando nivel aventureiro
     float quociente = (float) populacaoUm / areaUm; 
@@ -112,9 +86,9 @@ int main() {
     printf("Estado: %c \n", estadoDois);
     printf("Codigo: %s \n", codigoCartaDois);
     printf("Cidade: %s \n", nomeCidadeDois);
-    printf("População: %d \n", populacaoDois);
-    printf("Área: %f km² \n", areaDois);
-    printf("PIB: %f bilhões de reais \n", pibDois);
+    printf("População: %.2d \n", populacaoDois);
+    printf("Área: %.2f km² \n", areaDois);
+    printf("PIB: %.2f bilhões de reais \n", pibDois);
     printf("Número de pontos turisticos: %d \n", pontosTuristicosDois);
     //Nível aventureiro
     float quociente3 = (float) populacaoDois / areaDois; 
@@ -126,7 +100,7 @@ int main() {
     // Calcular PIB per capita (reais)
     // Além de exibir as informações lidas do usuário (como no nível básico), seu programa também deve exibir a densidade populacional e o PIB per capita calculados para cada cidade. Formate os valores de ponto flutuante com duas casas decimais.
 
-
+    // nivel Mestre:
 
     return 0;
 }
